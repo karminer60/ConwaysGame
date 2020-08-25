b57b9c86dd8692d81f0cf72fbef8edf8992cbf7a
Karina <GIT SERVICE EMAIL>
2020-08-25 15:43:38 -0700

    working on grid

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,3 +1,8 @@
-export default function Cards() {
-    
+import React, {useState, useEffect} from "react";
+
+export default function Grid() {
+    const [state, setState] = useState(initialState)
+    return(
+        
+    )    
 }
