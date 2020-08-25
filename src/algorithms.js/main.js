0c1461e8032fe7b2b795b218a54a69a84d25bbff
Karina <GIT SERVICE EMAIL>
2020-08-25 15:43:13 -0700

    working on grid

---
+++ src/algorithms.js/main.js
@@ -0,0 +1,3 @@
+//Rules for Conway's Game of Life:
+//if the cell is alive and has 2 or 3 neighbors then it remains alive. Else it dies
+//if cell is dead and has exactly 2 neighbors then it comes to life. Else it remains dead
